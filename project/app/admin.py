90e009f2bda143367e954a43eb503ea7f706ee50
Alexis Covarrubias <your_email@example.comalexis.covarrubias32@icloud.com>
2026-05-03 21:16:26 -0500

    fixed movielist dynamically

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Slider, Advertisement, SocialLink, Trailer, TrailerItem, Celebrity, News, Tweet, MovieTheater, MovieTV
-
+from .models import (Slider,Advertisement,SocialLink,Trailer,TrailerItem,Celebrity,News,Tweet,Movie)
 admin.site.register(Slider)
 admin.site.register(Advertisement)
 admin.site.register(SocialLink)
@@ -9,5 +8,4 @@ admin.site.register(TrailerItem)
 admin.site.register(Celebrity)
 admin.site.register(News)
 admin.site.register(Tweet)
-admin.site.register(MovieTheater)
-admin.site.register(MovieTV)
+admin.site.register(Movie)
